b9859a23f942e72b5215c369b3a72eeb19393949
jay45652 <sgcheon@naver.com>
2023-05-06 01:51:18 +0900

    TwoPointer

--- Algorithm/Practice/Algorithm_06/src/Main.java
+++ Algorithm/Practice/Algorithm_06/src/Main.java
@@ -5,13 +5,55 @@ import java.util.Arrays;
 
 public class Main {
     public static int[] forLoop(int[] arr, int target) {
+        // 구간을 기록
+        int[] result = {-1, -1};
 
-        return null;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                // target값을 찾으면 구간 리턴
+                if (sum == target) {
+                    result[0] = i;
+                    result[1] = j - 1;
+                    break;
+                }
+                // 못찾으면 계속 더하기
+                sum += arr[j];
+            }
+
+            // 이중 for문이라서 나갈 수 있게 두번 처리한것.
+            if (sum == target) {
+                break;
+            }
+        }
+
+        return result;
     }
 
     public static int[] twoPointers(int[] arr, int target) {
+        int p1 = 0;
+        int p2 = 0;
+        int sum = 0;
+        int[]  result = {-1, -1};
 
-        return null;
+        while (true) {
+            // sum이 target보다 많으면 sum에서 첫번째 포인터 값 뺀 후 한칸 앞으로 이동.
+            if (sum > target) {
+                sum -= arr[p1++];
+            } else if (p2 == arr.length) {  // 값을 못찾으면 p2가 끝까지 감.
+                break;
+            } else {    // 그외 계속 p2의 값을 옮겨주면서 구간 합 찾기.
+                sum += arr[p2++];
+            }
+
+            if (sum == target) {
+                result[0] = p1;
+                result[1] = p2 - 1;
+                break;
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
